e3ffa5afe01a0c25eb3970c6b6b83d3d613bf837
Jorge Bescos Gascon <jorge.bescos.gascon@oracle.com>
2022-04-14 08:26:51 +0200

    Fix inner classes
    Signed-off-by: Jorge Bescos Gascon <jorge.bescos.gascon@oracle.com>

--- example/src/es/tododev/example/Main.java
+++ example/src/es/tododev/example/Main.java
@@ -3,9 +3,25 @@ package es.tododev.example;
 public class Main {
 
     public static void main(String[] args) {
+//      System.out.println("Main replaced by classEnhancer2Code/replace");
+        System.out.println("You should not see this");
+        Inner.doNothing();
+        new Runnable(){
+            public void run() {
+//              System.out.println("Runnable replaced by classEnhancer2Code/replace");
+                System.out.println("You should not see this");
+            }
+        }.run();
         Info info = new Info();
         info.setText("Prefix ", "Hello world", 9);
         System.out.println(info.getText());
+    }
+    
+    private static class Inner {
+        private static void doNothing() {
+//          System.out.println("Main.Inner replaced by classEnhancer2Code/replace");
+            System.out.println("You should not see this");
+        }
     }
 
 }
